ff56e8d142d76c50ff1e3d97ebede278b220673a
imeoh <babyfox04@gmail.com>
2026-08-12 03:19:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ import qrcode
 app = Flask(__name__)
 
 database_url = os.getenv("DATABASE_URL", "sqlite:///orders.db")
+
 if database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+elif database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
